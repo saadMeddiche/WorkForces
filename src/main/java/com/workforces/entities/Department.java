345241d3aace2ced84f1982877578bb3a28c8dfe
saadMeddiche <saadmeddiche2004201@gmail.com>
2023-10-27 12:00:38 +0100

    Make Getters and Setters For Department and employe

--- src/main/java/com/workforces/entities/Department.java
+++ src/main/java/com/workforces/entities/Department.java
@@ -6,6 +6,7 @@ import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
+import java.util.Objects;
 
 public class Department {
 
@@ -20,5 +21,38 @@ public class Department {
 
     @NotEmpty(message = "Please enter description of department")
     private String description;
+
+    public Department() {
+    }
+
+    public Department(Long id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
 }
--- src/main/java/com/workforces/entities/Employe.java
+++ src/main/java/com/workforces/entities/Employe.java
@@ -30,4 +30,46 @@ public class Employe {
     @Valid
     private Department department;
 
+    public Employe() {
+    }
+
+    public Employe(Long id, FullName fullName, Salery salery, Department department) {
+        this.id = id;
+        this.fullName = fullName;
+        this.salery = salery;
+        this.department = department;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public FullName getFullName() {
+        return this.fullName;
+    }
+
+    public void setFullName(FullName fullName) {
+        this.fullName = fullName;
+    }
+
+    public Salery getSalery() {
+        return this.salery;
+    }
+
+    public void setSalery(Salery salery) {
+        this.salery = salery;
+    }
+
+    public Department getDepartment() {
+        return this.department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+
 }
